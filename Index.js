8c6114f2a1736ed4db36eb05692801d5ec3365a0
omar-faruk11 <omarabdullah1811@gmail.com>
2022-05-18 23:09:37 +0600

    Delete Notes added

--- Index.js
+++ Index.js
@@ -39,6 +39,15 @@ async function run (){
           const result = await noteCollection.insertOne(data);
           res.send(result)
       });
+      
+      app.delete('/notes/:id',async(req, res)=>{
+        const id = req.params.id;
+        const filter = {
+            _id: ObjectId(id)
+          }
+        const result = await noteCollection.deleteOne(filter)
+        res.send(result)
+    });
     }
     finally{
 
