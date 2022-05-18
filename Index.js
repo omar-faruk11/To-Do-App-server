d9bec416fc60731b806ed8cf8c607192ce8c19ec
omar-faruk11 <omarabdullah1811@gmail.com>
2022-05-18 22:33:38 +0600

    Get User's Notes

---
+++ Index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+
+const {
+    MongoClient,
+    ServerApiVersion,
+    ObjectId
+  } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+
+
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tiivm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1
+});
+
+async function run (){
+    try{
+        await client.connect();
+        const noteCollection = client.db("notes").collection("note");
+        console.log('connected');
+        app.get('/notes',async(req, res)=>{
+            const email = req.query.email;
+            const query = {email}
+            const notes = await noteCollection.find(query).toArray();
+            res.send(notes)
+        });
+    }
+    finally{
+
+    }
+}run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
